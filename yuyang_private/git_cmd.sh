15c1e3ecdabf3448dac071daffbf7a6aa2959ccb
yy1203 <43975805+yy1203@users.noreply.github.com>
2019-11-24 23:30:14 -0500

    Yuyang studies git. (#2)
    * Study git 1.
    
    * Study git 1.

---
+++ yuyang_private/git_cmd.sh
@@ -0,0 +1,23 @@
+# configure user and email
+
+
+# check branch
+git branch 
+
+# switch branch
+git checkout $branchname
+
+# create new branch and switch to new branch
+git checkout -b $branchname
+
+# load data from remote branch
+git pull origin master
+
+# upload to remote
+git push 
+
+# check the status of git?
+git status
+
+# check 
+git 
