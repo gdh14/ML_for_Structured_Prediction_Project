4c48a5d761ac12345c570b39b251a9d4e50ac84d
Yang Yu <yang@Yangs-MBP.wv.cc.cmu.edu>
2019-11-24 23:22:11 -0500

    Study git 1.

---
+++ yuyang_private/git_cmd.sh
@@ -0,0 +1,20 @@
+# check branch
+git branch 
+
+# switch branch
+git checkout $branchname
+
+# create new branch and switch to new branch
+git checkout -b $branchname
+
+# load data from remote branch
+git pull origin master
+
+# upload to remote
+git push 
+
+# check the status of git?
+git status
+
+# check 
+git 
